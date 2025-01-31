cae786f63b586d314143a4e9da088a6c082789f4
Nikhil22Feb-hub <vermanikhil2202@gmail.com>
2025-01-31 11:59:21 +0000

    Add source from environment
    Signed-off-by: Nikhil22Feb-hub <vermanikhil2202@gmail.com>

---
+++ app/login.js
@@ -0,0 +1,16 @@
+document.getElementById("login-form").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+
+    const validusername = "admin";
+    const validpassword = "admin";
+
+    const username = document.getElementById('UserName').value;
+    const password = document.getElementById('UserPassword').value;
+    if (username == validusername && password == validpassword){
+        alert("login successful");
+
+        window.location.href = "index.html";      //redirect to home page
+    }else{
+        alert("Invalid username or password. Please try again.");
+    }
+});
